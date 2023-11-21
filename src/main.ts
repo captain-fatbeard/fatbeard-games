0a5f043a2674e532bd08d9554f2133ccbe4431af
jjl <jjl@signifly.com>
2023-11-21 14:22:55 +0100

    fix: main.ts build

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import './style.css'
 import { Game } from 'phaser'
 import { config } from './config'
 
-const _game = new Game(config)
-_game.scene.start('main-scene')
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line no-new
+  new Game(config)
+})
