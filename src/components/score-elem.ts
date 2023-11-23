66002db35ca5551c3359a474c75589b57d64e109
jjl <jjl@signifly.com>
2023-11-23 09:50:35 +0100

    fix: hide score elem, until game is running

--- src/components/score-elem.ts
+++ src/components/score-elem.ts
@@ -16,18 +16,22 @@ export class ScoreElem {
     const scale = 0.5
 
     const elem = scene.scoreText = scene.add.text(gameDimentions.width - 150, 15, '0X', { fontSize: '40px', color: colors.blue })
-    elem.setVisible(true)
+    elem.setVisible(false)
     elem.setName('scoreElem')
 
-    const elem2 = scene.add.image(gameDimentions.width - (image.width / 2) / 2, (image.height * scale / 2) / 2, 'point') as Phaser.GameObjects.Image
-    elem2.setVisible(true)
-    elem2.setName('scoreElem')
-    elem2.setScale(scale)
+    const elemImage = scene.add.image(gameDimentions.width - (image.width / 2) / 2, (image.height * scale / 2) / 2, 'point') as Phaser.GameObjects.Image
+    elemImage.setVisible(false)
+    elemImage.setName('scoreElem')
+    elemImage.setScale(scale)
   }
 
   update(scene: MainScene) {
     const elem = scene.children.getByName('scoreElem') as Phaser.GameObjects.Text
     if (elem && scene.gameIsRunning)
       elem.setVisible(true)
+
+    const elemImage = scene.children.getByName('scoreElemImage') as Phaser.GameObjects.Text
+    if (elemImage && scene.gameIsRunning)
+      elemImage.setVisible(true)
   }
 }
