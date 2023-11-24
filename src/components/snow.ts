22dce9db5c0cd7ee297578c678f3b700c629256b
jjl <jjl@signifly.com>
2023-11-24 14:02:59 +0100

    fix: snow particles

--- src/components/snow.ts
+++ src/components/snow.ts
@@ -1,11 +1,10 @@
 import type { MainScene } from '../main-scene'
 
-// Assuming 'snowflake' is the correct key for your snowflake image
-import snowflake from './../assets/snow.png'
+import snow from './../assets/snow.png'
 
 export class Snow {
   preload(scene: MainScene) {
-    scene.load.image('snowflake', snowflake)
+    scene.load.image('snowflake', snow)
   }
 
   create(scene: MainScene) {
@@ -13,10 +12,10 @@ export class Snow {
 
     scene.add.particles(-200, 0, 'snowflake', {
       x: { min: 100, max: gameDimensions.width + 200 },
-      //   y: { min: 0, max: gameDimensions.height / 2 },
-      quantity: 1,
+      quantity: 0.5,
       lifespan: 5000,
       gravityY: 100,
+      scale: 1,
     })
   }
 }
